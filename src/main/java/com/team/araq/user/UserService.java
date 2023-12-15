73c96f324810a9554a8c5b3538434f34eb819da5
eejonghwa <teriyaki970326@gmail.com>
2023-12-15 14:48:15 +0900

    로그인 해야댐

---
+++ src/main/java/com/team/araq/user/UserService.java
@@ -0,0 +1,22 @@
+package com.team.araq.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public SiteUser craete(String username, String email, String password, String phoneNum){
+        SiteUser user = new SiteUser();
+        user.setUsername(username);
+        user.setEmail(email);
+        user.setPassword(passwordEncoder.encode(password));
+        user.setPhoneNum(phoneNum);
+        userRepository.save(user);
+        return user;
+    }
+}
