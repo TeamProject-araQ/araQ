33b116f4af6bd1be18b6320d8321c24f6135c3db
eejonghwa <130517498+eejonghwa@users.noreply.github.com>
2023-12-18 17:02:41 +0900

    Jh (#12)
    * asdf
    
    * signup
    
    * 1
    
    * 2
    
    * 업로드구현중
    
    * 111

--- src/test/java/com/team/araq/AraqApplicationTests.java
+++ src/test/java/com/team/araq/AraqApplicationTests.java
@@ -1,16 +1,10 @@
 package com.team.araq;
 
 import com.team.araq.user.UserService;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 
 @SpringBootTest
@@ -20,19 +14,6 @@ class AraqApplicationTests {
 
     @Test
 	void contextLoads() throws IOException {
-		FileInputStream file = new FileInputStream(new File("dummy.xlsx"));
-		Workbook workbook = new XSSFWorkbook(file);
 
-		Sheet sheet = workbook.getSheetAt(0);
-		for (Row row : sheet) {
-			if (row.getRowNum() == 0) continue;
-
-			userService.createTmp(row.getCell(0).getStringCellValue(),
-					String.valueOf((int)row.getCell(1).getNumericCellValue()),
-					row.getCell(2).getStringCellValue(),
-					row.getCell(3).getStringCellValue(),
-					row.getCell(4).getStringCellValue());
-		}
 	}
-
 }
