e8ae4cabb8b40e48aaf098ae55996997920edc01
njk7740 <njk7705@naver.com>
2023-12-19 15:59:32 +0900

    중간

--- src/main/java/com/team/araq/chat/Room.java
+++ src/main/java/com/team/araq/chat/Room.java
@@ -15,9 +15,9 @@ public class Room {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    @OneToOne
+    @ManyToOne
     private SiteUser participant1;
-    @OneToOne
+    @ManyToOne
     private SiteUser participant2;
     @OneToMany(mappedBy = "room", cascade = CascadeType.REMOVE)
     private List<Chat> chats;
