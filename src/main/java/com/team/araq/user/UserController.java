4f502bb0f66c003be22c925d9294f6e7e90ed921
eejonghwa <teriyaki970326@gmail.com>
2023-12-20 12:30:12 +0900

    123

--- src/main/java/com/team/araq/user/UserController.java
+++ src/main/java/com/team/araq/user/UserController.java
@@ -3,19 +3,22 @@ package com.team.araq.user;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.security.Principal;
+import java.util.HashMap;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @Controller
 @RequestMapping("/user")
 public class UserController {
     private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
 
     @GetMapping("/signup")
     public String signup(UserCreateForm userCreateForm){
@@ -48,5 +51,27 @@ public class UserController {
     @GetMapping("/login")
     public String login() {
         return "user/login";
+    }
+
+    @GetMapping("/update")
+    public String modify(){
+        return "user/update";
+    }
+
+    @PostMapping("/checkCurrentPw")
+    public Map<String, Boolean> checkCurrentPw(@RequestBody Map<String, String> request, Principal principal){
+        String currentPassword = request.get("currentPassword");
+        SiteUser user = userService.getByUsername(principal.getName());
+        // 예시로 현재 비밀번호가 "test123"일 때 true를 반환하도록 설정합니다.
+        boolean isValid = user != null && passwordEncoder.matches(currentPassword,user.getPassword());
+
+        Map<String, Boolean> response = new HashMap<>();
+        response.put("isValid", isValid);
+        return response;
+    }
+
+    @GetMapping("/updatePw")
+    public String checkPw(){
+        return "user/updatePw";
     }
 }
