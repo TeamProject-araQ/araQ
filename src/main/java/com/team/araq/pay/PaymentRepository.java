3e56f505f1873e5f7b9a3a2709cfa68321b5e86b
dbstjwjd <wjddbstj1@naver.com>
2023-12-20 17:49:24 +0900

    오늘치끝

--- src/main/java/com/team/araq/pay/PaymentRepository.java
+++ src/main/java/com/team/araq/pay/PaymentRepository.java
@@ -1,6 +1,12 @@
 package com.team.araq.pay;
 
+import com.team.araq.review.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PaymentRepository extends JpaRepository<Payment, Integer> {
+
+    Page<Payment> findAll(Specification<Payment> spec, Pageable pageable);
 }
