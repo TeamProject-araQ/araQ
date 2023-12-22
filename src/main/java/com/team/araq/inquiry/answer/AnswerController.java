00dbb55a9c37691104d65f212610c990b06d351e
dbstjwjd <wjddbstj1@naver.com>
2023-12-22 15:32:44 +0900

    중간커밋

--- src/main/java/com/team/araq/inquiry/answer/AnswerController.java
+++ src/main/java/com/team/araq/inquiry/answer/AnswerController.java
@@ -1,7 +1,11 @@
 package com.team.araq.inquiry.answer;
 
+import com.team.araq.inquiry.Inquiry;
+import com.team.araq.inquiry.InquiryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,4 +14,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AnswerController {
 
     private final AnswerService answerService;
+
+    private final InquiryService inquiryService;
+
+    @PostMapping("/create/{id}")
+    public String create(@PathVariable("id") Integer id, String content) {
+        Inquiry inquiry = this.inquiryService.getInquiry(id);
+        this.answerService.createAnswer(inquiry, content);
+        this.inquiryService.updateStatus(inquiry);
+        return "redirect:/admin/inquiry";
+    }
+
 }
