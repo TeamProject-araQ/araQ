3a9cd83ddeb3278348216b1f9810bd3dd95b4ccc
dbstjwjd <wjddbstj1@naver.com>
2023-12-29 15:10:48 +0900

    녹음도르마무

--- src/main/resources/static/record.js
+++ src/main/resources/static/record.js
@@ -1,54 +1,51 @@
 let mediaRecorder;
 let audioChunks = [];
 
-$(function () {
-    $("#startRecord").on('click', async function () {
-        try {
-            const stream = await navigator.mediaDevices.getUserMedia({audio: true});
-            mediaRecorder = new MediaRecorder(stream);
-            mediaRecorder.start();
+$(document).ready(function () {
+    $("#startRecord").click(async function () {
+        const stream = await navigator.mediaDevices.getUserMedia({audio: true});
+        mediaRecorder = new MediaRecorder(stream);
+        mediaRecorder.start();
 
-            audioChunks = [];
-            mediaRecorder.ondataavailable = event => {
-                audioChunks.push(event.data);
-            };
+        audioChunks = [];
+        mediaRecorder.ondataavailable = event => {
+            audioChunks.push(event.data);
+        };
 
-            mediaRecorder.onstop = async () => {
-                const audioBlob = new Blob(audioChunks);
-                await uploadAudio(audioBlob);
-            };
+        mediaRecorder.onstop = async () => {
+            const audioBlob = new Blob(audioChunks);
+            $("#audio").attr("src", URL.createObjectURL(audioBlob));
 
-            $("#stopRecord").prop("disabled", false);
-        } catch (error) {
-            console.error("오디오 장치를 찾을 수 없습니다: ", error);
-            alert("오디오 장치에 접근할 수 없습니다. 마이크가 연결되어 있는지 확인해주세요.");
-        }
+            await uploadAudio(audioBlob);
+        };
+
+        $("#stopRecord").prop("disabled", false);
     });
 
     $("#stopRecord").click(function () {
         mediaRecorder.stop();
         $(this).prop("disabled", true);
     });
-
-    const uploadAudio = async (audioBlob) => {
-        const audio = new FormData();
-        audio.append("audio", audioBlob, "voice.mp3");
-
-        $.ajax({
-            url: "/user/record",
-            type: "POST",
-            data: audio,
-            headers: {
-                [csrfHeader]: csrfToken
-            },
-            processData: false,
-            contentType: false,
-            success: function(response) {
-                alert(response);
-            },
-            error: function(error) {
-                alert(error);
-            }
-        });
-    };
 });
+
+const uploadAudio = async (audioBlob) => {
+    const formData = new FormData();
+    formData.append("audio", audioBlob, "voice.mp3");
+
+    $.ajax({
+        url: "/user/record",
+        type: "POST",
+        data: formData,
+        headers: {
+            [csrfHeader]: csrfToken
+        },
+        processData: false,
+        contentType: false,
+        success: function (response) {
+            alert(response);
+        },
+        error: function (error) {
+            alert(error);
+        }
+    });
+};
