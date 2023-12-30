9ca238bd08032694440371ba4d065b972740b920
dbstjwjd <wjddbtj1@naver.com>
2023-12-30 14:12:00 +0900

    약간수정

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -22,7 +22,9 @@ $(function () {
     const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]');
     const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
 
+    if ($('#postList').val() != null) {
     scrollToBottom();
+    }
 
     $(".voice").on('click', function () {
         var audio = $('.audio');
