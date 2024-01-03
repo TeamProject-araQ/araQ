e8b39e7d49da0691ca709fbb0b291e2f1d1ebac3
dbstjwjd <wjddbstj1@naver.com>
2024-01-03 10:44:41 +0900

    중간커밋

--- src/main/java/com/team/araq/taste/TasteController.java
+++ src/main/java/com/team/araq/taste/TasteController.java
@@ -1,9 +1,15 @@
 package com.team.araq.taste;
 
+import com.team.araq.user.SiteUser;
+import com.team.araq.user.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 @RequiredArgsConstructor
@@ -12,13 +18,22 @@ public class TasteController {
 
     private final TasteService tasteService;
 
+    private final UserService userService;
+
     @GetMapping("/main")
     public String main() {
         return "taste/main";
     }
 
     @GetMapping("/check")
-    public String check() {
+    public String check(TasteDTO tasteDTO) {
         return "taste/taste";
+    }
+
+    @PostMapping("/check")
+    public String check(@Valid TasteDTO tasteDTO, Principal principal) {
+        SiteUser user = this.userService.getByUsername(principal.getName());
+        this.tasteService.saveTaste(user, tasteDTO);
+        return "redirect:/";
     }
 }
