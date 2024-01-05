c46c6b40b8676e1b1fd0203294ad07addaf9de86
dbstjwjd <wjddbstj1@naver.com>
2024-01-05 17:14:26 +0900

    중간커밋

--- src/main/java/com/team/araq/user/UserService.java
+++ src/main/java/com/team/araq/user/UserService.java
@@ -413,4 +413,11 @@ public class UserService {
         user.setPersonality(personality);
         userRepository.save(user);
     }
+
+    public void deleteFriend(SiteUser user1, SiteUser user2) {
+        user1.getFriendList().remove(user2);
+        user2.getFriendList().remove(user1);
+        this.userRepository.save(user1);
+        this.userRepository.save(user2);
+    }
 }
