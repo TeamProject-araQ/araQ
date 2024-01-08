cb61d1f53e4fac1d7bd322c89c162ed2e3a8cab0
eejonghwa <130517498+eejonghwa@users.noreply.github.com>
2024-01-08 16:48:44 +0900

    Jh (#68)
    * 중간중간
    
    * 비밀번호변경
    
    * 중간
    
    * 아이디찾기
    
    * 회원가입
    
    * 회원가입시문자인증
    
    * 프로필수정사진업로드변경
    
    * 프로피수정
    
    * 사진지우기
    
    * 1234
    
    * 성격바꾸기
    
    * 성격수정
    
    * 소셜

--- src/main/java/com/team/araq/configuration/SecurityConfig.java
+++ src/main/java/com/team/araq/configuration/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.team.araq.CustomSuccessHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -38,6 +39,8 @@ public class SecurityConfig {
                         .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
                         .logoutSuccessUrl("/")
                         .invalidateHttpSession(true))
+                .oauth2Login((oauth2Login) -> oauth2Login.loginPage("/user/login")
+                        .defaultSuccessUrl("/"));
         ;
         return http.build();
     }
